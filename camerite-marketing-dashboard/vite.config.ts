37b082ba16d35a41a5e5c40ed09a8a0f2061449b
Camerite <marketing@camerite.com>
2025-11-18 14:01:41 -0300

    Delete camerite-marketing-dashboard directory

--- camerite-marketing-dashboard/vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  base: '/performance-camerite/', // 👈 ESSENCIAL PARA FUNCIONAR NO GITHUB PAGES
-  plugins: [react()],
-})
